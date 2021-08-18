9bfe52755041f8c7e7af7784eea2c2e06bd6a2b4
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 16:55:58 +0100

    Add app navigation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import {BrowserRouter as Router, Switch, Route, NavLink, Link} from 'react-router-dom';
+import Books from './components/books/Books';
+import Categories from './components/categories/Categories';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Books',
+    component: <Books />,
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: <Categories />,
+  },
+];
+
+const Nav = () => (
+  <nav>
+    <Link to="/">Bookstore</Link>
+    <ul>
+      {routes.map(({ name, path }) => <NavLink key={path} exact to={path}>{name}</NavLink>)}
+    </ul>
+  </nav>
+);
+
 const App = () => (
   <h1>Bookstore</h1>
 );
