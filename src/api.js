2e156c3a6b562de402e8656ffbe81b9cad5ae097
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 21:58:10 +0100

    Add create book function

--- src/api.js
+++ src/api.js
@@ -1,22 +1,27 @@
 const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const STORAGE_KEY = 'appId';
 
-const post = (endpoint) => fetch(`${BASE_URL}${endpoint}`, {
+const post = (endpoint, body = {}) => fetch(`${BASE_URL}${endpoint}`, {
   method: 'POST',
+  body: JSON.stringify(body),
 });
 
 export const createApp = async () => {
-  let appId = localStorage.getItem('appId');
+  let appId = localStorage.getItem(STORAGE_KEY);
 
   if (!appId) {
-    const response = await post('/apps/');
+    const response = await post('/apps');
 
     appId = response.text();
-    localStorage.setItem('appId', appId);
+    localStorage.setItem(STORAGE_KEY, appId);
   }
 };
 
-export const createBook = async () => {
+export const createBook = async (book) => {
+  const appId = localStorage.getItem(STORAGE_KEY);
+  const response = await post(`/apps/${appId}/books`, book);
 
+  return response.status === 201;
 };
 
 export default {};
