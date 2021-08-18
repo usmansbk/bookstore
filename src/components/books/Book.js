d7df2c2f472d65b70a5e2ebdeb086c6f27ed4dde
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 19:37:11 +0100

    Add book card

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,3 +1,23 @@
-const Book = () => <h1>Book</h1>;
+import PropTypes from 'prop-types';
+
+const Book = ({ book }) => {
+  const { title, genre, author } = book;
+
+  return (
+    <div>
+      <h4>{genre}</h4>
+      <h2>{title}</h2>
+      <h6>{author}</h6>
+    </div>
+  );
+};
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Book;
