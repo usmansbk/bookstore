495303679f00f71e148791af9622f072dc216eb0
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 21:21:42 +0100

    Clear form

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -1,19 +1,40 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
-const AddBook = ({ categories = [] }) => (
-  <section>
-    <h2>ADD NEW BOOK</h2>
-    <form>
-      <input placeholder="Book title" required />
-      <select>
-        {categories.map((category) => (
-          <option key={category} value={category}>{category}</option>
-        ))}
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </section>
-);
+const AddBook = ({ categories = [] }) => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState(categories[0]);
+
+  const handleTitle = (event) => setTitle(event.target.value);
+  const handleCategory = (event) => setCategory(event.target.value);
+  const handleSubmit = (event) => {
+    const book = {
+      id: nanoid(),
+      title,
+      category,
+    };
+    console.log(book);
+    setTitle('');
+    setCategory(categories[0]);
+    event.preventDefault();
+  };
+
+  return (
+    <section>
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={handleSubmit}>
+        <input value={title} placeholder="Book title" required onChange={handleTitle} />
+        <select value={category} onChange={handleCategory}>
+          {categories.map((category) => (
+            <option key={category} value={category}>{category}</option>
+          ))}
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </section>
+  );
+};
 
 AddBook.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.string).isRequired,
