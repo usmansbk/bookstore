bf9a5740390b5a75ec960abd91a13f70e32e588d
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 21:06:16 +0100

    Add book action creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,16 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
+const initialState = [];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
 
 export default reducer;
