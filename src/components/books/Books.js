6046703dd1740fd8615c69b92d02b26859c88bd0
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 19:28:08 +0100

    Add sample data

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,3 +1,23 @@
-const Books = () => <h1>Books</h1>;
+import Book from './Book';
+
+const books = [
+  {
+    genre: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    genre: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    genre: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+];
+
+const Books = () => <ul>{books.map((book) => <Book key={book.title} book={book} />)}</ul>;
 
 export default Books;
