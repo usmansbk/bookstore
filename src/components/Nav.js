61b850193953aa8226a2084567856d6827c67617
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 17:12:12 +0100

    Create navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import { Link, NavLink } from 'react-router-dom';
+
+const Nav = ({ title, routes }) => (
+  <nav>
+    <Link to="/">{title}</Link>
+    <ul>
+      {routes.map(({ name, path }) => <NavLink key={path} exact to={path}>{name}</NavLink>)}
+    </ul>
+  </nav>
+);
+
+Nav.propTypes = {
+  title: PropTypes.string.isRequired,
+  routes: PropTypes.arrayOf({
+    path: PropTypes.string.isRequired,
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+export default Nav;
