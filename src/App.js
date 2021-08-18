b7c795e6f8f93500b9811e62906cc6217504b84b
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 21:25:58 +0100

    Add store provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Books from './components/books/Page';
 import Categories from './components/categories/Categories';
 import Nav from './components/Nav';
+import store from './redux/configureStore';
 
 const routes = [
   {
@@ -17,14 +19,16 @@ const routes = [
 ];
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
-    <Nav title="Bookstore CMS" routes={routes} />
-    <Switch>
-      {routes.map(({ path, component }) => (
-        <Route path={path} exact key={path}>{component}</Route>
-      ))}
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Nav title="Bookstore CMS" routes={routes} />
+      <Switch>
+        {routes.map(({ path, component }) => (
+          <Route path={path} exact key={path}>{component}</Route>
+        ))}
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
