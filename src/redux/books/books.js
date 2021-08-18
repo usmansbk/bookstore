07c314439794723bc5dabd3764574012ee519a7b
Usman Suleiman <usmansbk@gmail.com>
2021-08-18 22:41:21 +0100

    Get books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,17 @@
 import * as API from '../../api';
 
+const LOAD_BOOKS = 'bookStore/books/LOAD_BOOKS';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
 API.createApp();
+
+const loadBooks = (payload) => ({
+  type: LOAD_BOOKS,
+  payload,
+});
 
 const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -32,12 +38,22 @@ export const deleteBook = (id) => async (dispatch) => {
   }
 };
 
+export const getBooks = () => async (dispatch) => {
+  const books = await API.getBooks();
+
+  if (books) {
+    dispatch(loadBooks(books));
+  }
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case LOAD_BOOKS:
+      return Object.values(action.payload).map(([book]) => book);
     default:
       return state;
   }
