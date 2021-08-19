a28c2c94bcd30766bcefbd4b194d7e548c94e7f2
Usman Suleiman <usmansbk@gmail.com>
2021-08-19 08:52:13 +0100

    Set initial data

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,38 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: '1',
+    category: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Chapter 17',
+      completed: '64',
+    },
+  },
+  {
+    id: '2',
+    category: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    progress: {
+      currentChapter: 'Chapter 3: "A Lesson Learned"',
+      completed: '8',
+    },
+  },
+  {
+    id: '3',
+    category: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Introduction',
+      completed: '0',
+    },
+  },
+];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
