5a6ac2517e63b20f55a2a11c440b9c7b3f3fc471
Usman Suleiman <usmansbk@gmail.com>
2021-08-19 10:25:31 +0100

    Add partial progress

--- src/components/books/CircularProgress.js
+++ src/components/books/CircularProgress.js
@@ -2,7 +2,6 @@ import './CircularProgress.css';
 
 const CircularProgress = () => (
   <div className="circular-progress-container">
-    <div className="circular-progress-negative" />
     <div className="circular-progress" />
   </div>
 );
