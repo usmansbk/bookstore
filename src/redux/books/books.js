3bec9dd1ee4e13d8856a7af2a4fe5e84334c7e6c
Usman Suleiman <usmansbk@gmail.com>
2021-08-19 08:39:43 +0100

    Fix delete book method

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,6 +2,7 @@ import * as API from '../../api';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const SET_BOOKS = 'bookStore/books/SET_BOOKS';
 
 const initialState = [];
 
@@ -15,6 +16,11 @@ const addBook = (payload) => ({
 const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
+});
+
+const setBooks = (payload) => ({
+  type: SET_BOOKS,
+  payload,
 });
 
 export const createBook = (book) => async (dispatch) => {
@@ -32,12 +38,33 @@ export const deleteBook = (id) => async (dispatch) => {
   }
 };
 
+export const loadBooks = () => async (dispatch) => {
+  const books = await API.getBooks();
+
+  if (books) {
+    dispatch(setBooks(books));
+  }
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case SET_BOOKS:
+      return Object.entries(action.payload).map(([key, value]) => {
+        const [book] = value;
+        return {
+          item_id: key,
+          ...book,
+          author: 'Suzanne Collins',
+          progress: {
+            currentChapter: 'Chapter 17',
+            completed: '64',
+          },
+        };
+      });
     default:
       return state;
   }
