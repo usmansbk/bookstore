58266648c04b8c965ab5b558dacbaf3efb679120
Usman Suleiman <usmansbk@gmail.com>
2021-08-19 08:51:52 +0100

    Rename books state

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
-  booksReducer,
+  books: booksReducer,
 });
 
 const store = createStore(
